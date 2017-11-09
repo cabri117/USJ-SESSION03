85d1b0f6d04b12b4cd011d4d6030370ae8c26d76
hawk <danielcabri@gmail.com>
2017-11-10 00:13:39 +0100

    LIST ERROR

--- app/src/main/java/com/example/hawk/imhungry/ListActivity.java
+++ app/src/main/java/com/example/hawk/imhungry/ListActivity.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -41,26 +42,13 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        checkGPSPermission();
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        assert locationManager != null;
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        listView = findViewById(R.id.listView);
-        JSONResourceReader reader = new JSONResourceReader(this.getResources(), R.raw.restaurants);
-        final List<Restaurant> jsonObj = reader.constructUsingGson();
-        ListAdapter la = new ListAdapter(this,R.layout.adapter_list,jsonObj,
-                location.getLatitude(), location.getLongitude());
-        listView.setAdapter(la);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Parcelable listParcelable = Parcels.wrap(jsonObj.get(position));
-                Intent i = new Intent(getApplicationContext(),RestaurantsDetailsActivity.class);
-                i.putExtra("RESTAURANT", listParcelable);
-                startActivity(i);
-
-            }
-        });
+        String requiredPermission = "android.permission.ACCESS_FINE_LOCATION";
+        int checkVal =this.checkCallingOrSelfPermission(requiredPermission);
+        if (checkVal==PackageManager.PERMISSION_GRANTED){
+            onInit();
+        } else {
+            checkGPSPermission();
+        }
 
     }
 
@@ -88,11 +76,64 @@ public class ListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void checkGPSPermission() {
+    public boolean checkGPSPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void onInit() {
+        checkGPSPermission();
+        double lat = 0.0;
+        double log = 0.0;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        assert locationManager != null;
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(location != null) {
+            lat = location.getLatitude();
+            log = location.getLongitude();
+        }
+        listView = findViewById(R.id.listView);
+        JSONResourceReader reader = new JSONResourceReader(this.getResources(), R.raw.restaurants);
+        final List<Restaurant> jsonObj = reader.constructUsingGson();
+        ListAdapter la = new ListAdapter(this,R.layout.adapter_list,jsonObj,
+                lat, log);
+        listView.setAdapter(la);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Parcelable listParcelable = Parcels.wrap(jsonObj.get(position));
+                Intent i = new Intent(getApplicationContext(),RestaurantsDetailsActivity.class);
+                i.putExtra("RESTAURANT", listParcelable);
+                startActivity(i);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    onInit();
+
+                } else {
+                   checkGPSPermission();
+                }
+            }
         }
     }
 }
